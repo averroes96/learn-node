368d33222c2595a9f849b7e5b6ac8fa8f1bf5aca
averroes96 <addavigner@gmail.com>
2023-01-15 00:24:01 +0100

    added JWT middleware to employees listing

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../../controllers/EmployeesController")
+const jwtMiddleware = require("../../middleware/JWTMiddleware")
 
 
-router.route("/").get(controller.list).post(controller.create)
+router.route("/").get(jwtMiddleware.verify, controller.list).post(controller.create)
 
 router.route("/:id").get(controller.retrieve).put(controller.update).delete(controller.remove)
 
